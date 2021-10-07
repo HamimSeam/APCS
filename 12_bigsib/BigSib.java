6344925436257d73a7daba9bfb09dcbbea3dce8a
Hamim Seam <91282842+HamimSeam@users.noreply.github.com>
2021-10-06 20:41:29 -0400

    Create BigSib.java

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,16 @@
+/*
+Gloria Lee and Hamim Seam
+APCS
+HW12: On Elder Individuality and the Elimination of Radio Fuzz/Java classes and objects/Using different methods within classes
+2021-10-5
+*/
+
+class BigSib{
+  public static void setHelloMsg(String a){
+    System.out.print(a);
+  }
+  public static String greet(String b){
+    b=" "+b;
+    return b;
+  }
+}
