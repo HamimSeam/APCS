5866981ffd3853d0fa955d53149ba5e89ea5d6b1
Hamim Seam <91282842+HamimSeam@users.noreply.github.com>
2021-10-06 20:52:02 -0400

    Create Greet.java

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,11 @@
+public class Greet {
+	public static void main(String[] args) {
+		greet("Gabriel");
+		greet("Hamim");
+		greet("Jonathan");
+	}
+	
+	public static void greet(String name) {
+		System.out.println("Why, hello there, " + name + ". How do you do?");
+	}
+}
