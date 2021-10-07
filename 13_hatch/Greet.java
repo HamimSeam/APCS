5a007ebf91103f8f76f016cb004bdfa8e0871205
Hamim Seam <91282842+HamimSeam@users.noreply.github.com>
2021-10-06 22:48:57 -0400

    Create Greet.java

---
+++ 13_hatch/Greet.java
@@ -0,0 +1,7 @@
+public class Greet{
+  public static void main(String[] args){
+    BigSib richard = new BigSib();
+    System.out.print(richard.greet("freshman"));
+    
+  } //end main()
+} //end Greet
