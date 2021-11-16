72758565c4e699195047d6e00b1276a0bff119fd
Hamim Seam <hseam30@stuy.edu>
2021-11-16 13:58:47 -0500

    arrays

---
+++ cb/array-2/CenteredAverage.java
@@ -0,0 +1,13 @@
+public class BigDiff {
+	public static int bigDiff(int[] nums) {
+		int max = 0;
+		int min = 99999;
+		int total = 0;
+		for (int i : nums) {
+			 if (i > max) max = i;
+			 if (i < min) min = i;
+			total += i;
+		}
+		return (total-max-min)/(nums.length-2);
+	}
+}
